17cf2690f6c49a6f0791d6e267cf4f984be3f70f
David <davidroussel171113@outlook.fr>
2025-02-18 21:45:50 +0100

    Ajout des user et transaction service, implémentation CommandLineRunner et test des services pour appel en BDD dans la méthode run()

--- src/main/java/com/payMyBuddy/PayMyBuddyApplication.java
+++ src/main/java/com/payMyBuddy/PayMyBuddyApplication.java
@@ -1,13 +1,46 @@
 package com.payMyBuddy;
 
+import com.payMyBuddy.model.Transaction;
+import com.payMyBuddy.model.User;
+import com.payMyBuddy.service.TransactionService;
+import com.payMyBuddy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+
 @SpringBootApplication
-public class PayMyBuddyApplication {
+public class PayMyBuddyApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private TransactionService transactionService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayMyBuddyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("L'application est lancer");
+
+		Iterable<User> users = userService.getUsers();
+		users.forEach(user -> System.out.println(user.getUsername()));
+
+		Iterable<Transaction> transactions = transactionService.getTransactions();
+		transactions.forEach(transaction -> System.out.println(transaction.getDescription()));
+
+		Optional<User> optionalUser = userService.getUserById(1);
+		User userId1 = optionalUser.get();
+		System.out.println(userId1.getUsername());
+
+		Optional<Transaction> optionalTransaction = transactionService.getTransactionById(1);
+		Transaction transactionId1 = optionalTransaction.get();
+		System.out.println(transactionId1.getDescription());
 	}
 
 }
