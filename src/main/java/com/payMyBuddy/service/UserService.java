a808a34914ff4dfa9169a510dee72c60b8f41529
David <davidroussel171113@outlook.fr>
2025-02-18 21:44:03 +0100

    Ajout de la couche service pour User

---
+++ src/main/java/com/payMyBuddy/service/UserService.java
@@ -0,0 +1,23 @@
+package com.payMyBuddy.service;
+
+import com.payMyBuddy.model.User;
+import com.payMyBuddy.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Iterable<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Integer id) {
+        return userRepository.findById(id);
+    }
+}
