403c36ad8e5808597bd52248171b62599fa5fd66
David <davidroussel171113@outlook.fr>
2025-02-23 23:30:35 +0100

    Ajout du repository du model UserFriends

--- src/main/java/com/payMyBuddy/repository/UserFriendsRepository.java
+++ src/main/java/com/payMyBuddy/repository/UserFriendsRepository.java
@@ -1,2 +1,9 @@
-package com.payMyBuddy.repository;public interface UserFriendsRepository {
+package com.payMyBuddy.repository;
+
+import com.payMyBuddy.model.UserFriends;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserFriendsRepository extends CrudRepository<UserFriends, Integer> {
 }
