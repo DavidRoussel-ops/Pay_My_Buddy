dbb0ebf821b966b61e202dedca945f099c017fbf
David <davidroussel171113@outlook.fr>
2025-02-23 23:33:06 +0100

    Ajout de la couche service du model UserFriends

---
+++ src/main/java/com/payMyBuddy/service/UserFriendsService.java
@@ -0,0 +1,28 @@
+package com.payMyBuddy.service;
+
+import com.payMyBuddy.model.User;
+import com.payMyBuddy.model.UserFriends;
+import com.payMyBuddy.repository.UserFriendsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserFriendsService {
+
+    @Autowired
+    UserFriendsRepository userFriendsRepository;
+
+    public Iterable<UserFriends> getUsers() {
+        return userFriendsRepository.findAll();
+    }
+
+    public Optional<UserFriends> getUserById(Integer id) {
+        return userFriendsRepository.findById(id);
+    }
+
+    public UserFriends addUserFriends(UserFriends userFriends) {
+        return userFriendsRepository.save(userFriends);
+    }
+}
