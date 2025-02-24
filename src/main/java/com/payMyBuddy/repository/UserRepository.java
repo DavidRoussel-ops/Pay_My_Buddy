eb88a9085b5fc52e2262b73f6cda9b58e4cba82c
David <davidroussel171113@outlook.fr>
2025-02-24 23:15:39 +0100

    Ajout d'une dérived query findByEmail()

--- src/main/java/com/payMyBuddy/repository/UserRepository.java
+++ src/main/java/com/payMyBuddy/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.payMyBuddy.repository;
 
 import com.payMyBuddy.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    public Iterable<User> findByEmail(String email);
+
 }
