0ec8be65fd9b99e187458f298fe02171e6deba0e
David <davidroussel171113@outlook.fr>
2025-02-28 15:43:49 +0100

    Modification pour extends JpaRepository

--- src/main/java/com/payMyBuddy/repository/UserRepository.java
+++ src/main/java/com/payMyBuddy/repository/UserRepository.java
@@ -1,13 +1,11 @@
 package com.payMyBuddy.repository;
 
 import com.payMyBuddy.model.User;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     public Iterable<User> findByEmail(String email);
 
