44172f71047a27afc219ae490b6d7a67227d3f30
David <davidroussel171113@outlook.fr>
2025-02-28 15:43:21 +0100

    Modification pour extends JpaRepository

--- src/main/java/com/payMyBuddy/repository/UserFriendsRepository.java
+++ src/main/java/com/payMyBuddy/repository/UserFriendsRepository.java
@@ -1,9 +1,9 @@
 package com.payMyBuddy.repository;
 
 import com.payMyBuddy.model.UserFriends;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserFriendsRepository extends CrudRepository<UserFriends, Integer> {
+public interface UserFriendsRepository extends JpaRepository<UserFriends, Integer> {
 }
