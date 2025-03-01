488629e8dec0847e24de8b7b463f16c760f8135b
David <davidroussel171113@outlook.fr>
2025-03-01 12:31:49 +0100

    Modification de la méthode filterChain()

--- src/main/java/com/payMyBuddy/configuration/SpringSecurityConfig.java
+++ src/main/java/com/payMyBuddy/configuration/SpringSecurityConfig.java
@@ -8,12 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,21 +20,10 @@ public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/login").permitAll()
-                .requestMatchers("/user")
-                .hasRole("USER")
-                .anyRequest().authenticated()
-        ).formLogin(form -> form
-                .loginPage("/login")
-                .defaultSuccessUrl("/index")
-                .permitAll()
-        ).logout(logout -> logout
-                .logoutUrl("logout")
-                .logoutSuccessUrl("/index")
-                .permitAll()
-        ).csrf(AbstractHttpConfigurer::disable);
-        return http.build();
+        return http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/user");
+            auth.anyRequest().authenticated();
+        }).formLogin(Customizer.withDefaults()).build();
     }
 
     @Bean
