669402abbadce3c0964056fae8d942c86dce2475
David <davidroussel171113@outlook.fr>
2025-03-05 22:55:06 +0100

    Modification de la méthode filterChain()

--- src/main/java/com/payMyBuddy/configuration/SpringSecurityConfig.java
+++ src/main/java/com/payMyBuddy/configuration/SpringSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -21,9 +22,16 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/user");
+            auth.requestMatchers("/login").permitAll();
+            auth.requestMatchers("/user").hasRole("USER");
             auth.anyRequest().authenticated();
-        }).formLogin(Customizer.withDefaults()).build();
+        }).formLogin(form -> {
+            form.loginPage("/login");
+            form.defaultSuccessUrl("/user", true).permitAll();
+        }).logout(logout -> {
+            logout.logoutUrl("/logout");
+            logout.logoutSuccessUrl("/login").permitAll();
+        }).csrf(AbstractHttpConfigurer::disable).build();
     }
 
     @Bean
