f6a9b79a1733cb428c89b8c93f18635d053d693b
David <davidroussel171113@outlook.fr>
2025-03-07 18:56:33 +0100

    Ajout de la couche service pour la sécurité de l'appli

---
+++ src/main/java/com/payMyBuddy/service/SecurityService.java
@@ -0,0 +1,47 @@
+package com.payMyBuddy.service;
+
+import com.payMyBuddy.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityService {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    public void updateSpringSecurityContext(User user) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(user.getEmail());
+        UsernamePasswordAuthenticationToken newAuthentication = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                authentication.getCredentials(),
+                userDetails.getAuthorities()
+        );
+        SecurityContextHolder.getContext().setAuthentication(newAuthentication);
+    }
+
+    public boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null && authentication.isAuthenticated();
+    }
+
+    public UserDetails getCurrentUserDetails() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object object = authentication.getPrincipal();
+            if (object instanceof UserDetails) {
+                return (UserDetails) object;
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+}
