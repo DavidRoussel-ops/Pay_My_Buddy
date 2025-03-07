a1276a5219e36dace2c9ea3c4f875c2af4a3439d
David <davidroussel171113@outlook.fr>
2025-03-07 18:50:29 +0100

    Ajout de la dépendence starter-test

--- src/main/java/com/payMyBuddy/controller/LoginController.java
+++ src/main/java/com/payMyBuddy/controller/LoginController.java
@@ -2,9 +2,11 @@ package com.payMyBuddy.controller;
 
 import com.payMyBuddy.configuration.SpringSecurityConfig;
 import com.payMyBuddy.model.User;
+import com.payMyBuddy.service.SecurityService;
 import com.payMyBuddy.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -15,10 +17,21 @@ public class UserController {
     private UserService userService;
 
     @Autowired
-    private SpringSecurityConfig springSecurityConfig;
+    private SecurityService securityService;
 
     @GetMapping("/user")
-    public String getUser() {
+    public String getUser(HttpSession session) {
+        if (!securityService.isAuthenticated()) {
+            System.out.println("Utilisateur non reconnu.");
+            return "login";
+        }
+        UserDetails userDetails = securityService.getCurrentUserDetails();
+        if (userDetails == null) {
+            System.out.println("Les détails utilisateur ne sont pas reconnu");
+            return "login";
+        }
+        User user = userService.getUserByEmail(userDetails.getUsername());
+        session.setAttribute("user", user);
         return "user";
     }
 
