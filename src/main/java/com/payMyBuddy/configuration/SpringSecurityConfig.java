a1dfd7c895f00a39010b8124bd83aca0c66339f6
David <davidroussel171113@outlook.fr>
2025-03-11 22:32:31 +0100

    Ajout base pour page de création de compte

--- src/main/java/com/payMyBuddy/configuration/SpringSecurityConfig.java
+++ src/main/java/com/payMyBuddy/configuration/SpringSecurityConfig.java
@@ -22,7 +22,7 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/login", "/registration").permitAll()
                         .requestMatchers("/user").hasRole("USER")
                         .anyRequest().authenticated()
                 )
