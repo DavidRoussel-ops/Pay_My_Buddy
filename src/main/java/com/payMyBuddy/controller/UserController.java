5b497f81ffd1c581a5d065e895088b95b32fcbd6
David <davidroussel171113@outlook.fr>
2025-03-13 20:27:49 +0100

    Finalisation de la méthode addUser()

--- src/main/java/com/payMyBuddy/controller/UserController.java
+++ src/main/java/com/payMyBuddy/controller/UserController.java
@@ -5,6 +5,7 @@ import com.payMyBuddy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -13,8 +14,16 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/registration")
-    public String addUser(User user) {
+    public String addUser(
+            @RequestParam(value = "username") String username,
+            @RequestParam(value = "email") String email,
+            @RequestParam(value = "password") String password
+    ) {
         try {
+            User user = new User();
+            user.setUsername(username);
+            user.setEmail(email);
+            user.setPassword(password);
             userService.addUser(user);
             System.out.println("Utilisateur enregister nom : " + user.getUsername() + " mail : " + user.getEmail() + " mot de passe " + user.getPassword() + " .");
             return "redirect:/login";
