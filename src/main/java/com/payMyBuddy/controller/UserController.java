c93cf92712c18625edccff3f4a83c6d95eb55825
David <davidroussel171113@outlook.fr>
2025-03-16 17:33:18 +0100

    Ajout de la méthode updateUser()

--- src/main/java/com/payMyBuddy/controller/UserController.java
+++ src/main/java/com/payMyBuddy/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.payMyBuddy.controller;
 
 import com.payMyBuddy.model.User;
+import com.payMyBuddy.service.SecurityService;
 import com.payMyBuddy.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +15,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private SecurityService securityService;
 
     @PostMapping("/registration")
     public String addUser(
@@ -30,6 +36,35 @@ public class UserController {
         } catch (Exception e) {
             System.out.println("e : " + e.getMessage());
             return "redirect:/registration";
+        }
+    }
+
+    @PostMapping("/profil")
+    public String updateUser(
+            @RequestParam(value = "username") String username,
+            @RequestParam(value = "email") String email,
+            @RequestParam(value = "password") String password,
+            HttpSession session
+    ) {
+        UserDetails userDetails = securityService.getCurrentUserDetails();
+        try {
+            User userExcisting = userService.getUserByEmail(userDetails.getUsername());
+            if (username != null && !username.isEmpty()) {
+                userExcisting.setUsername(username);
+            }
+            if (email != null && !email.isEmpty()) {
+                userExcisting.setEmail(email);
+            }
+            if (password != null && !password.isEmpty()) {
+                userExcisting.setPassword(password);
+            }
+            userService.addUser(userExcisting);
+            session.setAttribute("user", userExcisting);
+            securityService.updateSpringSecurityContext(userExcisting);
+            return "redirect:/profil";
+        } catch (Exception e) {
+            System.out.println("e : " + e.getMessage());
+            return "redirect:/profil";
         }
     }
 }
