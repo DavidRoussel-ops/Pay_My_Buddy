b42e8c19f05f57fe92949cba5d6f41121b8cf024
David <davidroussel171113@outlook.fr>
2025-03-25 21:21:56 +0100

    Ajout de LOG et modification return méthode getLogin()

--- src/main/java/com/payMyBuddy/controller/LoginController.java
+++ src/main/java/com/payMyBuddy/controller/LoginController.java
@@ -4,6 +4,8 @@ import com.payMyBuddy.model.User;
 import com.payMyBuddy.service.SecurityService;
 import com.payMyBuddy.service.UserService;
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
     private UserService userService;
@@ -23,23 +27,23 @@ public class LoginController {
         UserDetails userDetails = securityService.getCurrentUserDetails();
         User user = userService.getUserByEmail(userDetails.getUsername());
         session.setAttribute("user", user);
+        logger.info("Utilisateur : {}", user);
         return "profil";
     }
 
     @GetMapping("/login")
     public String getLogin(HttpSession session) {
         if (!securityService.isAuthenticated()) {
-            System.out.println("Utilisateur non reconnu.");
             return "login";
         }
         UserDetails userDetails = securityService.getCurrentUserDetails();
         if (userDetails == null) {
-            System.out.println("Les détails utilisateur ne sont pas reconnu");
             return "login";
         }
         User user = userService.getUserByEmail(userDetails.getUsername());
         session.setAttribute("user", user);
-        return "profil";
+        logger.info("Utilisateur connecter : {}", user);
+        return "transaction";
     }
 
     @GetMapping("/logout")
