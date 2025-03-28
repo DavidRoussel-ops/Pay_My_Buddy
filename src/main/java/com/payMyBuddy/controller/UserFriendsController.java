0dec10fdd2dccfe23428b677c8d8bc3cc9419fad
David <davidroussel171113@outlook.fr>
2025-03-28 09:57:03 +0100

    Modification controller pour allégement code

--- src/main/java/com/payMyBuddy/controller/UserFriendsController.java
+++ src/main/java/com/payMyBuddy/controller/UserFriendsController.java
@@ -1,18 +1,22 @@
 package com.payMyBuddy.controller;
 
 import com.payMyBuddy.model.User;
-import com.payMyBuddy.model.UserFriends;
 import com.payMyBuddy.service.SecurityService;
 import com.payMyBuddy.service.UserFriendsService;
 import com.payMyBuddy.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserFriendsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserFriendsController.class);
 
     @Autowired
     private UserService userService;
@@ -25,19 +29,22 @@ public class UserFriendsController {
 
     @PostMapping("/relationship")
     public String addUserFriends(
-            @RequestParam(value = "email") String email
+            @RequestParam(value = "email") String email,
+            RedirectAttributes redirectAttributes
     ) {
         UserDetails userDetails = securityService.getCurrentUserDetails();
         try {
             User userExcisting = userService.getUserByEmail(userDetails.getUsername());
             User userMail = userService.getUserByEmail(email);
-            UserFriends userFriends = new UserFriends();
-            userFriends.setUserId(userExcisting.getId());
-            userFriends.setUserFriends(userMail.getId());
-            userFriendsService.addUserFriends(userFriends);
+            if (userMail != null) {
+                userFriendsService.addUserFriends(userExcisting.getId(), userMail.getId());
+                logger.info("Email de l'utilisateur enregistrer en relation : {}", userMail.getEmail());
+            } else {
+                throw new IllegalArgumentException("L'adresse mail renseigner ne corresponds à aucune personnes connue de l'application !");
+            }
             return "redirect:/relationship";
-        } catch (Exception e) {
-            System.out.println("e : " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
             return "redirect:/relationship";
         }
     }
