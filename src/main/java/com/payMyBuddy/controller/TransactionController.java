fbddb1607f9ae6a23772192fc61288db46a31ab8
David <davidroussel171113@outlook.fr>
2025-03-28 09:56:04 +0100

    Modification controller pour allégement code

--- src/main/java/com/payMyBuddy/controller/TransactionController.java
+++ src/main/java/com/payMyBuddy/controller/TransactionController.java
@@ -5,14 +5,16 @@ import com.payMyBuddy.model.User;
 import com.payMyBuddy.service.SecurityService;
 import com.payMyBuddy.service.TransactionService;
 import com.payMyBuddy.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +22,8 @@ import java.util.Optional;
 
 @Controller
 public class TransactionController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionController.class);
 
     @Autowired
     private UserService userService;
@@ -69,21 +73,22 @@ public class TransactionController {
     public String populateList(
             @RequestParam(value = "email") String email,
             @RequestParam(value = "description") String description,
-            @RequestParam(value = "amount") double amount
+            @RequestParam(value = "amount", defaultValue = "0.0") Double amount,
+            RedirectAttributes redirectAttributes
     ) {
         UserDetails userDetails = securityService.getCurrentUserDetails();
         try {
             User userSender = userService.getUserByEmail(userDetails.getUsername());
             User userReceiver = userService.getUserByEmail(email);
-            Transaction transaction = new Transaction();
-            transaction.setSender(userSender.getId());
-            transaction.setReceiver(userReceiver.getId());
-            transaction.setDescription(description);
-            transaction.setAmount(amount);
-            transactionService.addTransaction(transaction);
-            return "redirect:/transaction";
-        } catch (Exception e) {
-            System.out.println("e : " + e.getMessage());
+            if (userReceiver != null) {
+                transactionService.addTransaction(userSender.getId(), userReceiver.getId(), description, amount);
+                logger.info("Information de la transaction : {} {} {}", userReceiver.getUsername(), description, amount);
+                return "redirect:/transaction";
+            } else {
+                throw new IllegalArgumentException("Veuillez selectionnez une relation valide.");
+            }
+        } catch (IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("erreur", e.getMessage());
             return "redirect:/transaction";
         }
     }
