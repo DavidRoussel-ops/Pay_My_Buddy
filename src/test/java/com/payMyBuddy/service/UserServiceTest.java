0f25ac90bd536556027478cef59e32f692949f25
David <davidroussel171113@outlook.fr>
2025-04-06 11:44:42 +0200

    Finalisation des tests de la class UserService

--- src/test/java/com/payMyBuddy/service/UserServiceTest.java
+++ src/test/java/com/payMyBuddy/service/UserServiceTest.java
@@ -4,20 +4,14 @@ import com.payMyBuddy.model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockHttpSession;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.ArrayList;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class UserServiceTest {
@@ -41,7 +35,7 @@ public class UserServiceTest {
             counter ++;
         }
         assertThat(allUsers).isNotNull();
-        Assertions.assertEquals(5, counter);
+        Assertions.assertEquals(4, counter);
     }
 
     @Test
@@ -87,10 +81,141 @@ public class UserServiceTest {
             lastId = user.getId();
             counter ++;
         }
+        counter --;
         Optional<User> users = userService.getUserById(lastId);
         User user = users.get();
         userService.deleteUserById(user.getId());
         assertThat(allUsers).isNotNull();
-        Assertions.assertEquals(5, counter);
+        Assertions.assertEquals(4, counter);
+    }
+
+    @Test
+    public void testFormValidationUsernameEmpty() throws IllegalArgumentException {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.formValidation("", "test", "test@gmail.com");
+        });
+        String expectedMessage = "Username ne peut être vide !";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testFormValidationPasswordEmpty() throws IllegalArgumentException {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.formValidation("test", "", "test@gmail.com");
+        });
+        String expectedMessage = "Le mot de passe ne peut être vide !";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testFormValidationMailEmpty() throws IllegalArgumentException {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.formValidation("test", "test", "");
+        });
+        String expectedMessage = "L'email ne peut être vide !";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testFormValidationUsernameNull() throws IllegalArgumentException {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.formValidation(null, "test", "test@gmail.com");
+        });
+        String expectedMessage = "Username ne peut être vide !";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testFormValidationPasswordNull() throws IllegalArgumentException {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.formValidation("test", null, "test@gmail.com");
+        });
+        String expectedMessage = "Le mot de passe ne peut être vide !";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testFormValidationMailNull() throws IllegalArgumentException {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.formValidation("test", "test", null);
+        });
+        String expectedMessage = "L'email ne peut être vide !";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testFormValidationMailExistInBDD() throws IllegalArgumentException {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.formValidation("test", "test", "michel@gmail.com");
+        });
+        String expectedMessage = "Cette email existe déjà !";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testFormUpdateValidationUsernameEmpty() throws IllegalArgumentException {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.formUpdateValidation("", "test", "test@gmail.com");
+        });
+        String expectedMessage = "Username ne peut être vide !";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testFormUpdateValidationPasswordEmpty() throws IllegalArgumentException {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.formUpdateValidation("test", "", "test@gmail.com");
+        });
+        String expectedMessage = "Le mot de passe ne peut être vide !";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testFormUpdateValidationMailEmpty() throws IllegalArgumentException {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.formUpdateValidation("test", "test", "");
+        });
+        String expectedMessage = "L'email ne peut être vide !";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testFormUpdateValidationUsernameNull() throws IllegalArgumentException {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.formUpdateValidation(null, "test", "test@gmail.com");
+        });
+        String expectedMessage = "Username ne peut être vide !";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testFormUpdateValidationPasswordNull() throws IllegalArgumentException {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.formUpdateValidation("test", null, "test@gmail.com");
+        });
+        String expectedMessage = "Le mot de passe ne peut être vide !";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testFormUpdateValidationMailNull() throws IllegalArgumentException {
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.formUpdateValidation("test", "test", null);
+        });
+        String expectedMessage = "L'email ne peut être vide !";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
     }
 }
