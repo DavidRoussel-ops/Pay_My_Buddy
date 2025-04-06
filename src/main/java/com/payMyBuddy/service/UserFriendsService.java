1e8786b7a06edb666c548cd88b699b5b5ed143f3
David <davidroussel171113@outlook.fr>
2025-04-06 11:47:08 +0200

    Ajout de la méthod deleteUserFriendsById() pour validation tests et ajout condition pour non enregistrement d'une connexion si elle existe déjà

--- src/main/java/com/payMyBuddy/service/UserFriendsService.java
+++ src/main/java/com/payMyBuddy/service/UserFriendsService.java
@@ -31,14 +31,24 @@ public class UserFriendsService {
 
     @Transactional
     public UserFriends addUserFriends(int userId, int userFriendsId) {
+        Iterable<UserFriends> userFriendsInBDD = getUserFriends();
         try {
             UserFriends userFriends = new UserFriends();
             userFriends.setUserId(userId);
             userFriends.setUserFriends(userFriendsId);
+            for (UserFriends userFriendInBDD : userFriendsInBDD) {
+                if (userFriends.getUserId() == userFriendInBDD.getUserId() && userFriends.getUserFriends() == userFriendInBDD.getUserFriends()) {
+                    throw new IllegalArgumentException("Vous avez déjà ajouter cette personne à votre liste de connexion.");
+                }
+            }
             logger.info("Relation bien enregistrer.");
             return userFriendsRepository.save(userFriends);
         } catch (IllegalArgumentException e){
             throw new IllegalArgumentException("La relation n'as pas était ajouter.");
         }
+    }
+
+    public void deleteUserFriendsById(Integer id) {
+        userFriendsRepository.deleteById(id);
     }
 }
